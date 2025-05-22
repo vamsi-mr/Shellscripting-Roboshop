ff8235a699005b077e2edf9694d0bb3547dfd898
Priyanka <priyanka.rel6@gmail.com>
2025-05-22 13:05:38 +0530

    created script file for MongoDB

--- frontend.sh
+++ frontend.sh
@@ -54,8 +54,7 @@ VALIDATE $? "Removing default content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
-rm -rf /usr/share/nginx/html/*
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
 
